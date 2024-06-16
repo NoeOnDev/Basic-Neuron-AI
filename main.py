820ee2297c7df98aac0a3c9f705d27e096033cc5
NoeOnDev <alxg5516@gmail.com>
2024-06-16 17:04:12 -0600

    feat: add data loading functionality to main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+dataset = pd.read_excel('2024.05.22 dataset 8A.xlsx')
+
+x1 = dataset[['x1']]
+x2 = dataset[['x2']]
+x3 = dataset[['x3']]
+x4 = dataset[['x4']]
+yd = dataset[['y']]
